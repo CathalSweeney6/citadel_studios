5844b70182a4d2fe3c02566d9ee7816f90ddd2e3
Cathal Sweeney <cjsweeney6@gmail.com>
2023-12-03 15:02:14 +0000

    add categories, products app  and fixtures

--- citadel_studios/settings.py
+++ citadel_studios/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'p@ei#69*b*zz3u4yie-$()@cy^l(+x9&@6ypx+r0lm(3%_9hr7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-cathalsween-citadelstud-n9j2ra0tl8e.ws-eu106.gitpod.io']
 
@@ -154,3 +154,5 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
