178553395ac074b84f233f04c037b26a47d30289
Cathal Sweeney <cjsweeney6@gmail.com>
2023-12-06 21:08:40 +0000

    create checkout app and models

--- citadel_studios/settings.py
+++ citadel_studios/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'p@ei#69*b*zz3u4yie-$()@cy^l(+x9&@6ypx+r0lm(3%_9hr7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-cathalsween-citadelstud-n9j2ra0tl8e.ws-eu106.gitpod.io']
+ALLOWED_HOSTS = ['8000-cathalsween-citadelstud-a9qxo0zyak9.ws-eu106.gitpod.io']
 
 
 # Application definition
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'home',
     'packages',
     'bag',
+    'checkout',
 
 ]
 
