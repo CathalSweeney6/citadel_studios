5c8e1c6bbe49a248ad0e2e1bf8288086757d2425
Cathal Sweeney <cjsweeney6@gmail.com>
2023-12-07 14:02:57 +0000

    add loading overlay

--- checkout/views.py
+++ checkout/views.py
@@ -4,7 +4,7 @@ from django.conf import settings
 
 from .forms import OrderForm
 from .models import Order, OrderLineItem
-from products.models import Product
+from packages.models import Product
 from bag.contexts import bag_contents
 
 import stripe
