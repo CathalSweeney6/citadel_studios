ea8d61936f71c999e1eaa23575d03cd1345afe56
Cathal Sweeney <cjsweeney6@gmail.com>
2023-12-09 10:29:06 +0000

    prepare for inital deployment

--- citadel_studios/settings.py
+++ citadel_studios/settings.py
@@ -13,6 +13,9 @@ import os
 import dj_database_url
 from pathlib import Path
 
+if os.path.isfile("env.py"):
+    import env
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
