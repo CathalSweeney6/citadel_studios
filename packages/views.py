6817a7451011f146a0b1ec0913f8dd63fafc1827
Cathal Sweeney <cjsweeney6@gmail.com>
2024-01-03 14:15:27 +0000

    add recording packages

--- packages/views.py
+++ packages/views.py
@@ -12,7 +12,7 @@ from .forms import ProductForm
 def all_products(request):
     """ A view to show all products, including sorting and search queries """
 
-    products = Product.objects.all()
+    products = Product.objects.all().order_by("-price")
     query = None
     categories = None
     sort = None
