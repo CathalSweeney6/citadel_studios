317fa1b438e70653d5ef4927f69fa71010b0b516
Cathal Sweeney <cjsweeney6@gmail.com>
2024-02-08 17:29:49 +0000

    fix bug with order history currecy

--- bag/apps.py
+++ bag/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
-
+#Python code for configuring bag
 class BagConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'bag'
--- bag/templatetags/bag_tools.py
+++ bag/templatetags/bag_tools.py
@@ -2,7 +2,7 @@ from django import template
 
 
 register = template.Library()
-
+#Displays quantity of item
 @register.filter(name='calc_subtotal')
 def calc_subtotal(price, quantity):
     return price * quantity
