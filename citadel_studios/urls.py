cffbc1d417c459f5ecd48ad441c8c4e9a7343601
Cathal Sweeney <cjsweeney6@gmail.com>
2024-02-11 18:43:25 +0000

    fix bug with empty bag 500 error

--- citadel_studios/urls.py
+++ citadel_studios/urls.py
@@ -17,7 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import handler404
+from django.conf.urls import handler404, handler500
+from .views import handler404, handler500
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -33,3 +34,4 @@ urlpatterns = [
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = "citadel_studios.views.handler404"
+handler500 = "citadel_studios.views.handler500"
